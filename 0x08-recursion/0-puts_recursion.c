4d006558f6c8e3b9bd072ea905135424d897a04e
lavinlavi <lavinkituyi@gmail.com>
2023-06-18 04:01:56 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include 'main.h'
+
+void _puts_recursion(char *s)
+{
+/**
+*Function that takes a string and passes each character to function _puthar
+*for output
+*/
+if (*s == '\0')
+{
+_putchar("\n");
+return;
+}
+else
+{
+_putchar(*s);
+s = s + 1;
+_puts_recursion(s);
+}
+}
