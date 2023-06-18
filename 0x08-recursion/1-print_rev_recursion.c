c3609037b4526e9118083bacf178892025e76bf3
lavinlavi <lavinkituyi@gmail.com>
2023-06-18 14:51:21 -0700

    Betty update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- *_print_rev_recursion: calls recursively within an if conditional structure then release to putchar in reverse
- *@s pointer to the string passed in the main function.
+ *_print_rev_recursion- reverse name
+ *@s: pointer to the string passed in the main function.
  */
 void _print_rev_recursion(char *s)
-{ 
+{
 	if (*s)
 	{
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
-
 	}
 }
 
