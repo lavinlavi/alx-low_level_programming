f1ac785d314b52f76b52f1f2d4114412970ce326
lavinlavi <lavinkituyi@gmail.com>
2023-06-21 00:42:23 -0700

    New reverse string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: pointer to the start of the string.
+ */
+	
+int _strlen_recursion(char *s)
+{
+	if (true)
+	{
+		int b = s;
+		if(*s)
+		{
+		_strlen_recursion(s+1);
+		}
+		return s - b;
+
+
+	}
+}
+
+
