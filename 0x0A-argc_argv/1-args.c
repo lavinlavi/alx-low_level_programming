a27cf4aabec6ddb048271b9a159d06b94adf0af2
lavinlavi <lavinkituyi@gmail.com>
2023-06-27 04:12:17 -0700

    new task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -the main function of the problem.
+ * @argc: the number of the arguments passed into the command line.
+ * @argv: the argument vector
+ *
+ * Return: returns 0 always to show success.
+ *
+ */
+
+int main(int argc, char __attribute__((unused))**argv)
+{
+	printf("%d", argc);
+	printf("\n");
+
+	return (0);
+
+}
+
