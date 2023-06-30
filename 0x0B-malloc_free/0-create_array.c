e638d0aef481d8b369dace3ea9b2c6bbd73425ec
lavinlavi <lavinkituyi@gmail.com>
2023-06-30 01:31:56 -0700

    New task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars and initializes
+ * that array to a given character.
+ * @size: size of the array to be created.
+ * @c: the character to be used for initialization.
+ *
+ * Return: returns the pointer to the array if success or NULL if failure.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = NULL;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (ptr);
+	}
+
+	ptr = (char *) malloc(size * (sizeof(char)));
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+char *create_array(unsigned int size, char c);
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif
