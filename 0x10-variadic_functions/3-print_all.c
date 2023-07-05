f46cbd392853581f5b38b69fbc69d7a9a63330d0
lavinlavi <lavinkituyi@gmail.com>
2023-07-05 06:49:58 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,52 @@
 #include "variadic_functions.h"
 
 /**
- * display_all - Show any datatype.
- * @format: The list of datatypes passed to this function.
+ * print_all - Outputs any data type.
+ * @format: A string of characters denoting the argument types.
  *
  * No return value.
  */
-
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *types = "cifs";
-    
-    va_start(args, format);
-    char *output_str;
-    for (const char* itr = format; *itr != '\0'; ++itr)
-    {
-        for (int j = 0; types[j] != '\0'; ++j)
-        {
-            if (*itr == types[j] && count > 0)
-            {
-                printf(", ");
-            }
-        }
-        count = 1;
-        switch (*itr)
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                output_str = va_arg(args, char *);
-                printf("%s", output_str ? output_str : "(nil)");
-                break;
-        }
-    }
-    printf("\n");
-    va_end(args);
-}
+	va_list arg_list;
+	unsigned int index_main = 0, index_secondary, flag_comma = 0;
+	char *text;
+	const char types[] = "cifs";
 
+	va_start(arg_list, format);
+	while (format && format[index_main])
+	{
+		index_secondary = 0;
+		while (types[index_secondary])
+		{
+			if (format[index_main] == types[index_secondary] && flag_comma)
+			{
+				printf(", ");
+				break;
+			} index_secondary++;
+		}
+		switch (format[index_main])
+		{
+		case 'c':
+			printf("%c", va_arg(arg_list, int)), flag_comma = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(arg_list, int)), flag_comma = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arg_list, double)), flag_comma = 1;
+			break;
+		case 's':
+			text = va_arg(arg_list, char *), flag_comma = 1;
+			if (!text)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", text);
+			break;
+		} index_main++;
+	}
+	printf("\n"), va_end(arg_list);
+}
 
