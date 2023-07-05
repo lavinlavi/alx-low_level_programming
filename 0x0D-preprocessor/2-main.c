eb31cca4e0461032d7adb427d022a6280f6e19c5
lavinlavi <lavinkituyi@gmail.com>
2023-07-05 13:03:52 -0700

    Correction

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_file_name - prints the name of the current file
+ *
+ * Return: Always 0 (Success)
+ */
+int print_file_name(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
