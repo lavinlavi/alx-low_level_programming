0425a32453dfed2ede24dd9cbf11944a4acdda64
lavinlavi <lavinkituyi@gmail.com>
2023-07-05 12:39:33 -0700

    0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main
- *
- * Desc: Central header file
- */
-int _putchar(char c);  // Prints a single character
-void *malloc_checked(unsigned int b);  // Allocates memory and checks for successful allocation
-char *string_nconcat(char *s1, char *s2, unsigned int n);  // Concatenates n characters from string s2 to string s1
-void *_calloc(unsigned int nmemb, unsigned int size);  // Allocates zero-initialized memory for nmemb elements of size bytes each
-int *array_range(int min, int max);  // Creates an array of integers from min to max
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);  // Resizes the memory block pointed to by ptr from old_size to new_size
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
-
