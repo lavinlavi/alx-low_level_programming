10ade62769b6f928bd428c257a07e9ee170970ce
lavinlavi <lavinkituyi@gmail.com>
2023-07-05 13:10:45 -0700

    Dog C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Structure representing a dog's basic information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+struct dog *new_dog(char *name, float age, char *owner);
+void free_dog(struct dog *d);
+
+#endif
+
