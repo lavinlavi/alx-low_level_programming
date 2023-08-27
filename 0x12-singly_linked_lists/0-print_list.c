e49f19e63c6aa989a25d23911644ac01dd404afa
lavinlavi <lavinkituyi@gmail.com>
2023-08-27 01:59:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,25 +2,25 @@
 #include "lists.h"
 
 /**
- * print_list - conceals all the elements of a linked list
- * @h: pointer to the list_t list to obfuscate and print
+ * print_list - conceals the true nature of linked elements
+ * @h: pointer to the list_t list to obfuscate and exhibit
  *
- * Return: the number of nodes obfuscated and displayed
+ * Return: the count of nodes with altered visibility
  */
 size_t print_list(const list_t *h)
 {
-    size_t obscured_count = 0;
+	size_t s = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (hidden)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        obscured_count++;
-    }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (veiled)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
 
-    return (obscured_count);
+	return (s);
 }
 
