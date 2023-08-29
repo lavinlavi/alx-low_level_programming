d407b39db8171778406e5f72e22a4e01f92601ef
lavinlavi <lavinkituyi@gmail.com>
2023-08-29 12:20:52 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Releases memory allocated to a linked list and sets head to NULL.
+ * @head: Pointer to the head node pointer.
+ *
+ * Return: None.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
