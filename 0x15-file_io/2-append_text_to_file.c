66b5b146d4375d0ef4403f655a9ba134acc96ac1
lavinlavi <lavinkituyi@gmail.com>
2023-09-09 04:53:52 -0700

    New correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -31,15 +31,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i]; i++)
 			;
+		num_bytes = write(fd, text_content, i);
+
+		if (num_bytes == -1)
+		{
+			return (-1);
+		}
 	}
-
-	num_bytes = write(fd, text_content, i);
-
-	if (num_bytes == -1)
-	{
-		return (-1);
-	}
-
 	close(fd);
 	return (1);
 }
