fd1923027352012ef35667b3bb05a4930db22720
lavinlavi <lavinkituyi@gmail.com>
2023-09-09 08:28:02 -0700

    New commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * main - main function to copy from one file to another
+ *
+ * @argc: the number of arguments passed to main
+ * @argv: a pointer to the arguments.
+ *
+ * Return: return 0 on success
+ */
+
+
+int main(int argc, char **argv)
+{
+	int fd_1, fd_2;
+	char buf[BUF_SIZ];
+	ssize_t num_bytes;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n");
+		exit(97);
+	
+	}
+	if (!argv[1])
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP |S_IROTH);
+	fd_2 = open(argv[1], O_RDONLY);
+
+	if (fd_1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(100);
+	}
+
+	while ((num_bytes = read(fd_2, buf, BUF_SIZ)) > 0)
+	{
+		if (write(fd_1, buf, num_bytes) != num_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1);
+		exit(100);
+	}
+	if (close(fd_2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2);
+		exit(100);
+	}
+	return (0);
+}	
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,3 +11,7 @@ int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
+
+#ifndef BUF_SIZ
+#define BUF_SIZ 1024
+#endif
