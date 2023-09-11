ea46c2caeb703db77c2d6dd028bf1381a0fd8a7d
lavinlavi <lavinkituyi@gmail.com>
2023-09-11 08:04:53 -0700

    New update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-void read_to_buf(char *buf, int fd_1, int fd_2, char *arg_2);
+void read_to_buf(char *buf, int fd_1, int fd_2, char *arg_2, char *arg_1);
 
 /**
  * main - main function to copy from one file to another
@@ -14,7 +14,7 @@ void read_to_buf(char *buf, int fd_1, int fd_2, char *arg_2);
 int main(int argc, char **argv)
 {
 	int fd_1, fd_2, i, j;
-	char buf[BUF_SIZ];
+	char *buf;
 
 	if (argc != 3)
 	{
@@ -22,23 +22,13 @@ int main(int argc, char **argv)
 		exit(97);
 
 	}
-	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR
-			| S_IRGRP | S_IWGRP | S_IROTH);
-	fd_2 = open(argv[1], O_RDONLY);
-
-	if (fd_1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(100);
-	}
-	read_to_buf(buf, fd_1, fd_2, argv[2]);
+	buf = malloc(sizeof(char) * 1024);
+	fd_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_1 = open(argv[1], O_RDONLY);
+	read_to_buf(buf, fd_1, fd_2, argv[2], argv[1]);
 	i = close(fd_1);
 	j = close(fd_2);
+
 	if (i == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1);
@@ -57,20 +47,38 @@ int main(int argc, char **argv)
  * @buf: buffer pointer
  * @fd_1: file descriptor
  * @fd_2: - file descriptor
+ * @arg_1: argument 1
  * @arg_2: - argument 2
  */
 
-void read_to_buf(char *buf, int fd_1, int fd_2, char *arg_2)
+void read_to_buf(char *buf, int fd_1, int fd_2, char *arg_2, char *arg_1)
 {
 	ssize_t num_bytes;
 
-	while ((num_bytes = read(fd_2, buf, BUF_SIZ)) > 0)
-	{
-		if (write(fd_1, buf, num_bytes) != num_bytes)
+	num_bytes = read(fd_1, buf, 1024);
+
+	do {
+		if (fd_1 == -1 || num_bytes == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s \n", arg_2);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", arg_1);
+			free(buf);
+			exit(98);
+		}
+
+		num_bytes = write(fd_2, buf, num_bytes);
+
+		if (num_bytes == -1 || fd_2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg_2);
+			free(buf);
 			exit(99);
 		}
-	}
+
+		num_bytes = read(fd_1, buf, 1024);
+		fd_2 = open(arg_2, O_WRONLY | O_APPEND);
+
+	} while (num_bytes > 0);
+
+	free(buf);
 }
 
