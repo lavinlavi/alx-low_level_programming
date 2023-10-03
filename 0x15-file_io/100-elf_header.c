a36fbaa7050735b68ec0ac3dc1ac01d382213206
root <lavinkituyi@gmail.com>
2023-10-02 18:39:55 -0700

    Corretions

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,8 @@
+/*
+ * File: 100-elf_header.c
+ * Auth: Lavin Kituyi
+ */
+
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,84 +11,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Function Prototypes */
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-/* ... [rest of your prototypes, not included for brevity] */
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
-/* Check if file is ELF; exit 98 if not */
-void check_elf(unsigned char *e_ident)
-{
-    int index;
-    /* ... [rest of the function] */
+/**
+ * check_elf - Verify if file is an ELF file.
+ * @e_ident: ELF magic numbers.
+ * Description: Exits with code 98 if not an ELF file.
+ */
+void check_elf(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header magic numbers, space-separated */
-void print_magic(unsigned char *e_ident)
-{
-    int index;
-    /* ... [rest of the function] */
+/**
+ * print_magic - Display ELF header magic numbers.
+ * @e_ident: ELF magic numbers.
+ * Description: Numbers are space-separated.
+ */
+void print_magic(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header class */
-void print_class(unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_class - Display class of ELF header.
+ * @e_ident: ELF class.
+ */
+void print_class(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header data */
-void print_data(unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_data - Display data of ELF header.
+ * @e_ident: ELF class.
+ */
+void print_data(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header version */
-void print_version(unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_version - Display ELF header version.
+ * @e_ident: ELF version.
+ */
+void print_version(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header OS/ABI */
-void print_osabi(unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_osabi - Display OS/ABI of ELF header.
+ * @e_ident: ELF version.
+ */
+void print_osabi(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header ABI version */
-void print_abi(unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_abi - Display ELF header ABI version.
+ * @e_ident: ELF ABI version.
+ */
+void print_abi(unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header type */
-void print_type(unsigned int e_type, unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_type - Display type of ELF header.
+ * @e_type: ELF type.
+ * @e_ident: ELF class.
+ */
+void print_type(unsigned int e_type, unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Print ELF header entry point address */
-void print_entry(unsigned long int e_entry, unsigned char *e_ident)
-{
-    /* ... [rest of the function] */
+/**
+ * print_entry - Display ELF header entry point.
+ * @e_entry: ELF entry point address.
+ * @e_ident: ELF class.
+ */
+void print_entry(unsigned long int e_entry, unsigned char *e_ident) {
+    // ... (rest of the function)
 }
 
-/* Close ELF file; exit 98 if fail */
-void close_elf(int elf)
-{
-    if (close(elf) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", elf);
-        exit(98);
-    }
+/**
+ * close_elf - Close ELF file.
+ * @elf: File descriptor of ELF file.
+ * Description: Exits with code 98 if cannot close.
+ */
+void close_elf(int elf) {
+    // ... (rest of the function)
 }
 
-/* Display info in ELF header at ELF file start */
-int main(int __attribute__((__unused__)) argc, char *argv[])
-{
-    Elf64_Ehdr *header;
-    int o, r;
-    /* ... [rest of the function] */
-    return (0);
+/**
+ * main - Display ELF header info.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success, 98 on failure.
+ * Description: Exits with code 98 on errors.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[]) {
+    // ... (rest of the function)
 }
 
